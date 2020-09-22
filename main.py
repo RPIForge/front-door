dfa3b1bbc161494d4c58aa94d76121763c026001
Michael Honaker <mchonaker@gmail.com>
2020-09-22 10:23:47 -0400

    Updated error message

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ while(True):
     try:
         variable_object.slack_class.start()
     except:
-        self.variable_object.logger_class.logger.exception("Unable to read calendar")
+        variable_object.logger_class.logger.exception("Unable to read calendar")
